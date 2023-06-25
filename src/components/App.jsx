e5284948498e2492a6118268e8546a9de2e25ebc
Miles Mosweu <tmtmosweu@gmail.com>
2023-06-25 10:37:45 +0200

    Install sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import Attribution from './Attribution'
-import '../styles/App.css'
+import '../styles/App.scss'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <main className='app'>      
+    <main className='app'>
+      <h1>Sass successfully added!</h1>    
       <Attribution />
     </main>
   )
