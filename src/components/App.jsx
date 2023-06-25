6c71b66e84921997344491a0010af90c8a3dcfbb
Miles Mosweu <tmtmosweu@gmail.com>
2023-06-25 10:23:52 +0200

    Set up vaiables and styles

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Attribution from './Attribution'
+import '../styles/App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <main className='app'>      
+      <Attribution />
+    </main>
+  )
+}
+
+export default App
