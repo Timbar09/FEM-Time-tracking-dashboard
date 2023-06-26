e181bd00eaa9e8b9b1ff7f57bebf9498bafcaf2b
Miles Mosweu <tmtmosweu@gmail.com>
2023-06-26 13:39:24 +0200

    Update sass installation

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {sassPlugin} from 'vite-plugin-sass'
+import sassPlugin from 'vite-plugin-sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
